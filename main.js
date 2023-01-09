bd01785a3cf116849af865cf3ef46a77df6c8973
Tuan Anh <121181732+ntuananh1124@users.noreply.github.com>
2023-01-09 14:58:05 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var abc = "xua nay chua tung thay";
+var def = "mot nha tu hinh trai tym <3 !"
+var dcm = "thay nha tu hinh trai tym bao gio chua ?"
+
+prompt('Nhap so tuoi cua ban !');
+console.log('day la log'); //in ra console
+confirm(dcm);
+alert (abc);
+alert (def);
